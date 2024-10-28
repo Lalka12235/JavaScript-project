081736bf33fdc97e3156df5402e57237c4b37a8d
Egor <61911209+Lalka12235@users.noreply.github.com>
2024-10-28 22:08:13 +0700

    Movie info

---
+++ Movie generation/script.js
@@ -0,0 +1,60 @@
+const key = '271b508b';
+
+const input = document.getElementById('movie');
+const result = document.getElementById('result');
+const search = document.getElementById('search');
+
+function getMovie(){
+    let movieName = input.value;
+    let url = `http://www.omdbapi.com/?t=${movieName}&apikey=${key}&lang=ru`;
+    //if input field is empty
+
+    if (movieName.length <= 0) {
+        result.innerHTML = `<h3 class="msg">Please enter a movie name </h3>`;
+    }
+
+    //if input isn't empty
+    else {
+        fetch(url).then((resp) => resp.json()).then((data) => {
+            //if movie exist in database
+            if (data.Response == "True") {
+                result.innerHTML = `
+                    <div class="info">
+                        <img src=${data.Poster} class="poster">
+                        <div>
+                            <h2>${data.Title}</h2>
+                            <div class="rating">
+                                <img src="star-icon.svg">
+                                <h4>${data.imdbRating}</h4>
+                            </div>
+                            <div class="details">
+                                <span>${data.Rated}</span>
+                                <span>${data.Year}</span>
+                                <span>${data.Runtime}</span>
+                            </div>
+                            <div class="genre">
+                                <div>${data.Genre.split(",").join("</div><div>")}</div>
+                            </div>
+                        </div>
+                    </div>
+                    <h3>Plot:</h3>
+                    <p>${data.Plot}</p>
+                    <h3>Cast:</h3>
+                    <p>${data.Actors}</p>
+                `;
+            }
+
+            //if movie doesn't exist in database
+            else {
+                result.innerHTML = `<h3 class="msg">${data.Error}</h3>`;
+            }
+        })
+            //if error occurs
+            .catch(() => {
+                result.innerHTML = `<h3 class="msg">Error Occured</h3>`;
+            });
+    }
+}
+
+search.addEventListener('click',getMovie);
+window.addEventListener("load", getMovie);
